32180f4291c30fcc573c8a754d716bbe7370e582
vchimania <vinichimania98@gmail.com>
2021-04-01 10:44:17 +0530

    merged

--- fb-clone-ang/src/app/app-routing.module.ts
+++ fb-clone-ang/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FbGuard } from './fb.guard';
+import { LoginComponent } from './login/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    loadChildren:() => import('./login/login.module').then(m => m.LoginModule),
+    canActivate:[FbGuard]
+  },{
+    path:'login',
+    component:LoginComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- fb-clone-ang/src/app/app.module.ts
+++ fb-clone-ang/src/app/app.module.ts
@@ -3,14 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+// import { MaterialModule } from '@angular/material';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    // MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
