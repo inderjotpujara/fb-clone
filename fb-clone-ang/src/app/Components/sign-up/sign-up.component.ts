5e97bc79bdacec316142c4b71c347a6cbc5835b1
Vaishak07 <vaishak.c@trukker.com>
2021-04-06 11:00:03 +0530

    Sign up page implementation

--- fb-clone-ang/src/app/Components/sign-up/sign-up.component.ts
+++ fb-clone-ang/src/app/Components/sign-up/sign-up.component.ts
@@ -1,38 +1,35 @@
 
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl,Validators} from '@angular/forms';
+import { Validators, FormBuilder, FormGroup, FormControl } from '@angular/forms';
 import { FacebookGuard } from 'src/app/facebook.guard';
 import { LoginComponent } from '../login/login.component';
-import {Router} from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
   styleUrls: ['./sign-up.component.css'],
-  providers:[LoginComponent,FacebookGuard]
+  providers: [LoginComponent, FacebookGuard]
 })
 export class SignUpComponent implements OnInit {
-  
-  constructor(private comp:LoginComponent,
-    private router:Router) { }
-  
-  
+
+  constructor(private comp: LoginComponent,
+    private router: Router, private fb: FormBuilder) { }
+
+
   ngOnInit(): void {
-    this.getDays();
     this.getYears();
     this.getCurrentMonthName();
+    this.getNumberOfDays();
   }
-  public days: any[] = []; 
+  public days: any[] = [];
   public years: any[] = [];
-  public selectionDay: any;
-  public selectionMonth: any;
-  public selectionYear:any;
-  public currentYear=(new Date()).getFullYear();
-  public currentDay=(new Date()).getDate();
-  public currentMonth=(new Date()).getMonth()+1;
-  public currentMonthName:any;
-  male ="Male";
-  female ="Female";
+  public currentYear = (new Date()).getFullYear();
+  public currentDay = (new Date()).getDate();
+  public currentMonth = (new Date()).getMonth() + 1;
+  public currentMonthName: any;
+  male = "Male";
+  female = "Female";
   public months: any[] = [
     {
       monthId: 1,
@@ -82,40 +79,72 @@ export class SignUpComponent implements OnInit {
       monthId: 12,
       monthName: 'Dec',
     }];
-  signUpForm = new FormGroup({
-    firstName: new FormControl('', Validators.required),
-    surName: new FormControl(''),
-    emailAddress: new FormControl(''),
-    newPassword: new FormControl(''),
-    day: new FormControl(''),
-    month: new FormControl(''),
-    year: new FormControl(''),
-    gender: new FormControl(''),
+
+  signUpForm = this.fb.group({
+    firstName: [null, Validators.required],
+    surName: [null, Validators.required],
+    emailAddress: [null, Validators.required],
+    newPassword: [null, Validators.required],
+    day: [this.currentDay, Validators.required],
+    month: [this.currentMonth, Validators.required],
+    year: [this.currentYear, Validators.required],
+    gender: [null, Validators.required],
+    imgUrl: [null],
   });
   get firstName() {
     return this.signUpForm.get('firstName');
   }
-  getDays(){
-    for (let i = 1; i <= 31; i++) {
-      this.days.push(i);
-    }
+  get surName() {
+    return this.signUpForm.get('surName');
   }
-  getYears(){
-    for (let i = this.currentYear; i >=1905 ; i--) {
+  get emailAddress() {
+    return this.signUpForm.get('emailAddress');
+  }
+  get newPassword() {
+    return this.signUpForm.get('newPassword');
+  }
+  get gender() {
+    return this.signUpForm.get('gender');
+  }
+  getYears() {
+    for (let i = this.currentYear; i >= 1905; i--) {
       this.years.push(i);
     }
   }
-  getCurrentMonthName(){
+  getCurrentMonthName() {
     let monthData = this.months.filter((m) => m.monthId == this.currentMonth);
-    this.currentMonthName=monthData[0].monthName;
+    this.currentMonthName = monthData[0].monthName;
   }
-  onSignUp()
-  {
+  onSignUp() {
+    if(this.signUpForm.valid){
     alert(JSON.stringify(this.signUpForm.value));
+    }else{
+      this.validateAllFields(this.signUpForm)
+    }
   }
-  closeSignUp(){
+  closeSignUp() {
     this.router.navigate(['\login'])
-    console.log("close signup")
+  }
+  validateAllFields(formGroup: FormGroup) {
+    Object.keys(formGroup.controls).forEach((field) => {
+      const control = formGroup.get(field);
+      if (control instanceof FormControl) {
+        control.markAsTouched({ onlySelf: true });
+      } else if (control instanceof FormGroup) {
+        this.validateAllFields(control);
+      }
+    });
+  }
+  getNumberOfDays() {
+    let Month=this.signUpForm.value.month;
+    let Year=this.signUpForm.value.year
+    this.days=[];
+    let month = Month;
+    let year = Year
+    let noOfDays = new Date(year, month, 0).getDate();
+    for (let i = 1; i <= noOfDays; i++) {
+      this.days.push(i);
+    }
   }
 }
 
