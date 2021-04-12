73d784250fa345dce73cbd89c39af9e94ae9613c
vchimania <vinichimania98@gmail.com>
2021-04-12 16:39:10 +0530

    Profile changes

--- fb-clone-ang/src/app/Components/profile/profile.component.ts
+++ fb-clone-ang/src/app/Components/profile/profile.component.ts
@@ -8,10 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
   Router: any;
+  isInfoEditing=false
+  profileInfo=`Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid
+  quam numquam inventore minima blanditiis earum repudiandae.`;
 
   constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  infoChanged(){
+console.log(this.profileInfo);
+
   }
 
 }
